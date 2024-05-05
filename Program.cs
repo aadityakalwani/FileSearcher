bcd739cb76954dec7898c5fbb2769120b11d239e
Aadi <aadityakalwani@gmail.com>
2024-05-05 20:44:25 +0100

    imported my getValidInt function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello World!");
+﻿internal abstract class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Running Program");
+    }
+    
+    public int GetValidInt(int min, int max)
+    {
+        if (int.TryParse(Console.ReadLine(), out int validInt))
+        {
+            if (validInt >= min && validInt <= max)
+            {
+                return validInt;
+            }
+
+            Console.WriteLine($"No bro enter a number between {min} and {max}");
+            return GetValidInt(min, max);
+        }
+
+        Console.WriteLine("Please enter a valid integer");
+        return GetValidInt(min, max);
+    }
+}
